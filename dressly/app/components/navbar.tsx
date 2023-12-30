dd4e4e711bbe9bdf4188aebde59da81025e9144e
Friday Joshua <fridayjoshua.dev@gmail.com>
2023-12-30 13:46:27 +0100

    working on the navbar

--- dressly/app/components/navbar.tsx
+++ dressly/app/components/navbar.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-// import Logo from '../'
+import Logo from '../logo.png';
 
 export default function Navbar() {
     return (
-        <div className="flex justify-between items-center bg-transparent h-7 fixed top-0 w-screen">
-            {/* <Image src={}/> */}
-        </div>
+        <nav className="flex justify-between items-center bg-transparent h-7 fixed top-0 w-screen">
+            <Image src={Logo} width={24} height={24} alt="Dressly logo"/>
+        </nav>
     );
 }
--- dressly/app/page.tsx
+++ dressly/app/page.tsx
@@ -2,19 +2,13 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import {Hamburger} from '@phosphor-icons/react'
+import {LineSegments} from '@phosphor-icons/react'
+import Navbar from './components/navbar'
 
 export default function Page(){
   return(
     <>
-     <nav className='w-full h-14 shadow-slate-800 transition-shadow duration-150 bg-secondary border-b-2 border-w-500 p-8 flex flex-row justify-between items-center space-x-8'>
-          <div>
-            <Hamburger size={24} className='text-accent'/>
-          </div>
-          <div className='flex space-x-8 justify-between items-center'>
-      
-          </div>
-        </nav>
+    <Navbar/>
     </>
   )
 }
